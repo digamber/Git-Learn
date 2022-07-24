b9567a5674c8de4fc5cd627ab871dcfd7bccdd1e
digamber <singh.knit@gmail.com>
2022-07-24 13:25:20 +0530

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,17 +1,12 @@
 class Main{
 
     public static void main (String args[]){
-
-<<<<<<< HEAD
         System.out.println("Hello World..");
         System.out.println("comming change again ");// this is change for commit and push it 
 
-        // Hellos digamber 
-
-=======
+        // Hellos digamber
         System.out.println("Hello world....");
 
         // this has to merge with branch feature/pts002
->>>>>>> 61bd54146c0fdce8e3db7608b398347f8a6410ca
     }
 }
